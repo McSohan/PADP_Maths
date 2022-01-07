8cf35c39687ffedb2dfa552dd38278ebe3faa697
McSohan <60097620+McSohan@users.noreply.github.com>
2022-01-07 10:57:20 +0530

    Update convolution.cu

--- 05_convolution/1d_tiled/convolution.cu
+++ 05_convolution/1d_tiled/convolution.cu
@@ -1,7 +1,6 @@
 // This program implements a 1D convolution using CUDA,
 // and stores the mask in constant memory, and loads
 // reused values into shared memory (scratchpad)
-// By: Nick from CoffeeBeforeArch
 
 #include <cassert>
 #include <cstdlib>
